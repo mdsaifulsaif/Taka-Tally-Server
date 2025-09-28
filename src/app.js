06bf1efe116e79b06c3d4c7fe79c69ad80a2473a
Md Saiful <saifulislam66info@gmail.com>
2025-09-29 01:28:20 +0600

    set frontend domain

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://taka-tally.netlify.app"],
     credentials: true,
   })
 );
